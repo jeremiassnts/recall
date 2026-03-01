42959e7b83c94db0032141ed3ff5e1f1bca63fc7
Jeremias Santos <jeremiassnts3@gmail.com>
2026-03-01 20:29:39 -0300

    feat: add skeleton loading in metrics

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useListDashboardMetrics } from "@/api/metrics/useListDashboardMetrics";
 import { MetricCard } from "@/components/MetricCard";
+import { Skeleton } from "@/components/ui/skeleton";
 import { CircleCheck, CircleDollarSign, CircleOff, Send } from "lucide-react";
 
 export default function DashboardPage() {
@@ -8,12 +9,20 @@ export default function DashboardPage() {
   console.log(metrics);
 
   return (
-    <div className="grid grid-cols-4 gap-4 px-10">
-      <MetricCard label="Total applications" value={metrics?.total ?? 0} icon={<Send className="w-3 h-3 text-white" />} />
-      <MetricCard label="Active" value={metrics?.active ?? 0} icon={<CircleCheck className="w-3 h-3 text-white" />} />
-      <MetricCard label="Rejected" value={metrics?.rejected ?? 0} icon={<CircleOff className="w-3 h-3 text-white" />} />
-      <MetricCard label="Offers" value={metrics?.offers ?? 0} icon={<CircleDollarSign className="w-3 h-3 text-white" />} />
-    </div>
+    <>
+      {isLoading && <div className="grid grid-cols-4 gap-4 px-10">
+        <Skeleton className="h-[150px] bg-gray-200 dark:bg-gray-700" />
+        <Skeleton className="h-[150px] bg-gray-200 dark:bg-gray-700" />
+        <Skeleton className="h-[150px] bg-gray-200 dark:bg-gray-700" />
+        <Skeleton className="h-[150px] bg-gray-200 dark:bg-gray-700" />
+      </div>}
+      {!isLoading && <div className="grid grid-cols-4 gap-4 px-10">
+        <MetricCard label="Total applications" value={metrics?.total ?? 0} icon={<Send className="w-3 h-3 text-white" />} />
+        <MetricCard label="Active" value={metrics?.active ?? 0} icon={<CircleCheck className="w-3 h-3 text-white" />} />
+        <MetricCard label="Rejected" value={metrics?.rejected ?? 0} icon={<CircleOff className="w-3 h-3 text-white" />} />
+        <MetricCard label="Offers" value={metrics?.offers ?? 0} icon={<CircleDollarSign className="w-3 h-3 text-white" />} />
+      </div>}
+    </>
     // <div>
     //   <h1 className="text-xl font-semibold text-neutral-900 dark:text-white">
     //     Dashboard
