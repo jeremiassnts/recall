c3cc5f5162bbd0f77d97e43bd67a605fbb0b17b6
Jeremias Santos <jeremiassnts3@gmail.com>
2026-03-01 20:03:13 -0300

    feat: logout button on avatar

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useUser } from "@auth0/nextjs-auth0";
+import { LogOutIcon } from "lucide-react";
 import { ThemeToggle } from "./ThemeToggle";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 export function Header() {
     const { user } = useUser();
@@ -10,13 +13,22 @@ export function Header() {
             <h1 className="text-lg font-bold">Dashboard</h1>
             <div className="flex flex-row justify-center items-center gap-4">
                 <ThemeToggle />
-                <Avatar>
-                    <AvatarImage
-                        src={user?.picture ?? ""}
-                        alt={user?.name ?? ""}
-                    />
-                    <AvatarFallback>{user?.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
-                </Avatar>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon" className="rounded-full">
+                            <Avatar>
+                                <AvatarImage src={user?.picture ?? ""} alt={user?.name ?? ""} />
+                                <AvatarFallback>{user?.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
+                            </Avatar>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end" className="border-[1px] border-gray-200 dark:border-gray-800 rounded-md">
+                        <DropdownMenuItem>
+                            <LogOutIcon />
+                            <a href="/auth/logout">Sign Out</a>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </header>
     );
